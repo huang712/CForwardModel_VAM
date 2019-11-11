566fa65b65fc97dd23afe895f2894ca45e0a41c0
Feixiong Huang <huang712@cygnsslnx1.ecn.purdue.edu>
2019-11-11 16:40:12 -0500

    2019-11-11 from linux

--- gitpush.sh
+++ gitpush.sh
@@ -2,6 +2,6 @@ NOW=$(date +"%Y-%m-%d")
 
 git rm -r --cached .
 git add .
-git commit -m "${NOW} add sh"
+git commit -m "${NOW} from linux"
 git remote add origin https://huang712@github.com/huang712/CForwardModel_VAM.git 
 git push origin master
